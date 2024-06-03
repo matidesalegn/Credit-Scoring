eedcf17f6accd067caf1fccb7eb036da3e301ec3
MatiwosD <matiwos.desalegn@hst-et.com>
2024-06-03 20:46:31 +0300

    first commit

---
+++ src/logger.py
@@ -0,0 +1,12 @@
+# src/logger.py
+import logging
+
+def setup_logger(name='credit_scoring_logger', log_file='credit_scoring.log', level=logging.INFO):
+    handler = logging.FileHandler(log_file)        
+    handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s %(message)s'))
+
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    logger.addHandler(handler)
+
+    return logger
---
+++ tests/test_data_loader.py
@@ -0,0 +1,24 @@
+# tests/test_data_loader.py
+import unittest
+from src.data_loader import DataLoader
+
+class TestDataLoader(unittest.TestCase):
+    def setUp(self):
+        self.loader = DataLoader('data/raw/dataset.csv')
+
+    def test_load_data(self):
+        data = self.loader.load_data()
+        self.assertIsNotNone(data)
+
+    def test_check_data_types(self):
+        data = self.loader.load_data()
+        dtypes = self.loader.check_data_types(data)
+        self.assertIsNotNone(dtypes)
+
+    def test_check_missing_values(self):
+        data = self.loader.load_data()
+        missing_values = self.loader.check_missing_values(data)
+        self.assertIsNotNone(missing_values)
+
+if __name__ == '__main__':
+    unittest.main()
